1f0d68eaf5d43ec4c30d1bcf2b03ad7ef7998efa
Alex Collins <alex@cloudcade.com>
2018-09-28 14:01:40 -0700

    Initial Commit

---
+++ handler.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const request = require('sync-request');
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
+
+exports.lambda_test = function(event, context, callback) {
+  const body = JSON.parse(event.body);
+  const { repository, sender } = body;
+
+  const repo = repository.name;
+
+  try {
+    run(repo);
+  } catch (err) {
+    console.log(err);
+    callback(err);
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Event Processed"
+    })
+  };
+  callback(null, response);
+};
+
+function run(repo) {
+  console.log(repo);
+}
